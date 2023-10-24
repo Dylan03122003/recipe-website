ed36525505e021b74aa2391cf9abaeab9344d6f6
Thuan <Thaun@gmail.com>
2023-10-24 16:01:43 +0700

    finished categories-instargam-UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import Trendy from "./sections/Trendy";
 import Learnmore from "./sections/Learnmore";
+import Categories from "./sections/Categories";
+import Instagram from "./sections/Instagram";
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <Trendy />
+      <Categories />
+      <Instagram />
       <Learnmore />
     </>
   );
