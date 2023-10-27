413fbd4d0d1d8788924fe0417517bf0245c23a31
hai duong <hduong7.hstnq@gmail.com>
2023-10-27 14:06:27 +0700

    finnished recipe

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Trendy from "./sections/Trendy";
 import Learnmore from "./sections/Learnmore";
 import Categories from "./sections/Categories";
 import Instagram from "./sections/Instagram";
+import TastyRecipe from "./sections/TastyRecipe";
+import DeliciousRecipe from "./sections/DeliciousRecipe";
 const App = () => {
   return (
     <>
@@ -11,8 +13,10 @@ const App = () => {
       <Hero />
       <Trendy />
       <Categories />
+      <TastyRecipe />
       <Instagram />
       <Learnmore />
+      <DeliciousRecipe />
     </>
   );
 };
