270241517960da7d005965a0963c46bf39b01cad
Quoc Duong <caodangquocduong2000ht@gmail.com>
2023-11-08 17:51:14 +0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import MoreRecipeDetail from "./pages/MoreRecipeDetail";
+import MoreRecipes from "./pages/MoreRecipes";
 import Blog from "./sections/Blog";
 import BlogList from "./sections/BlogList";
 import RecipeDetail from "./sections/RecipeDetail";
@@ -11,6 +13,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/more-recipes" element={<MoreRecipes />} />
+        <Route path="/more-recipes/:recipeID" element={<MoreRecipeDetail />} />
         <Route path="/recipes/:recipeID" element={<RecipeDetail />} />
         <Route path="/blogs" element={<BlogList />} />
         <Route path="/blogs/:blogID" element={<Blog />} />
